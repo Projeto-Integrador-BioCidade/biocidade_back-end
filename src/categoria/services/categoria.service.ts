54a3da3f1135817d8ed23711f9f47fb7f37cd8f1
lucas_souza_ribeiro <lucas.srs.2022@gmail.com>
2024-07-30 15:03:22 -0300

    adição de relations nos métodos find de categoria

--- src/categoria/services/categoria.service.ts
+++ src/categoria/services/categoria.service.ts
@@ -12,7 +12,11 @@ export class CategoriaService {
   ) {}
 
   async findAll(): Promise<Categoria[]> {
-    return await this.categoriaRepository.find();
+    return await this.categoriaRepository.find({
+      relations: {
+        produto: true
+      }
+    });
   }
 
   async findById(id: number): Promise<Categoria>{
@@ -20,6 +24,9 @@ export class CategoriaService {
     let buscarCategoria = await this.categoriaRepository.findOne({
       where:{
         id
+      },
+      relations: {
+        produto: true
       }
     })
 
@@ -35,6 +42,9 @@ export class CategoriaService {
     return await this.categoriaRepository.find({
       where:{
         nome: ILike(`%${nome}%`)
+      },
+      relations: {
+        produto: true
       }
     });
   }
