9a8e1d2be343a475fac6d958707daf067309a9fc
lucas_souza_ribeiro <lucas.srs.2022@gmail.com>
2024-08-06 12:23:07 -0300

    finalização da task7

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Categoria } from './categoria/entities/categoria.entity';
 import { CategoriaModule } from './categoria/categoria.module';
 import { ProdutoModule } from './produto/produto.module';
-import { Produto } from './produto/entities/produto.entity';
-import { Usuario } from './usuario/entities/usuario.entity';
 import { AuthModule } from './auth/auth.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { AppController } from './app.controller';
+import { ProdService } from './auth/data/services/prod.service';
+import { ConfigModule } from '@nestjs/config';
+import { DevService } from './auth/data/services/dev.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_biocidade',
-      entities: [Categoria, Produto, Usuario],
-      synchronize: true,
-    }),
+    ConfigModule.forRoot(),
+TypeOrmModule.forRootAsync({
+	useClass: ProdService,
+    imports: [ConfigModule],
+}),
     CategoriaModule,
     ProdutoModule,
     AuthModule,
