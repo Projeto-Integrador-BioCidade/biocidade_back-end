bffea03cf2df69b9dcbd5e35c801236ac9f44b02
Gabrielli <gabivirgilio@oultook.com>
2024-08-06 11:49:09 -0300

    Prod e Dev Service

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,6 @@ async function bootstrap() {
 
   app.enableCors(); //Habilitamos requisições de outras origens (Servdores)
 
-  await app.listen(4000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
